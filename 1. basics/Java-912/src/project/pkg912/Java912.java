1276f8de3c241362122cd2164c21040d4d4c4cca
Eslam.khder <eslamkhder11@gmail.com>
2026-07-01 22:57:29 +0300

    912

---
+++ 1. basics/Java-912/src/project/pkg912/Java912.java
@@ -0,0 +1,22 @@
+package project.pkg912;
+
+import java.util.Scanner;
+
+public class Java912 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int ids[] = new int[5];
+
+        ids[0] = 50;
+        ids[1] = 60;
+        ids[2] = 70;
+        ids[3] = 80;
+        ids[4] = 90;
+        
+        System.out.println("==> " + ids[4]);
+    }
+
+}
